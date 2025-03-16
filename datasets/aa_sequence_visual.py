f765f9b39732073b5615e4ab78a833ad12691ea8
shiipl <gm18305280617@gmail.com>
2025-03-16 12:09:46 +0800

    update

--- datasets/aa_sequence_visual.py
+++ datasets/aa_sequence_visual.py
@@ -13,35 +13,35 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 
 
-def read_data(df):
-    sequences = []
-    for index, row in df.iterrows():
-        sequence = SeqRecord(Seq(row['vh']), id=str(index))
-        sequences.append(sequence)
-    return sequences
+def generate_seq_records(dataframe):
+    record_list = []
+    for idx, data_row in dataframe.iterrows():
+        seq_entry = SeqRecord(Seq(data_row['vh']), id=str(idx))
+        record_list.append(seq_entry)
+    return record_list
 
-def alignment_process(sequences):
-    with open("temp.fasta", "w") as temp_file:
-        for sequence in sequences:
-            temp_file.write(f">{sequence.id}\n")
-            temp_file.write(str(sequence.seq) + "\n")
+def perform_alignment(seq_records):
+    with open("temp.fasta", "w") as tmp_file:
+        for seq_entry in seq_records:
+            tmp_file.write(f">{seq_entry.id}\n")
+            tmp_file.write(str(seq_entry.seq) + "\n")
     
-    align_file_cmd = "muscle5.1.linux_intel64 -align temp.fasta -output aln.fasta"
-    os.system(align_file_cmd)
-    alignment = AlignIO.read("aln.fasta", "fasta")
-    return alignment
+    alignment_command = "muscle5.1.linux_intel64 -align temp.fasta -output aln.fasta"
+    os.system(alignment_command)
+    alignment_result = AlignIO.read("aln.fasta", "fasta")
+    return alignment_result
 
-def build_df(alignment):
-    sequences = []
-    for record in alignment:
-        sequences.append(str(record.seq))
-    return sequences
+def extract_aligned_sequences(aligned_data):
+    aligned_seqs = []
+    for alignment_record in aligned_data:
+        aligned_seqs.append(str(alignment_record.seq))
+    return aligned_seqs
 
-def create_figure(sequence_list, output_filename):
-    counts_df = logomaker.alignment_to_matrix(sequence_list, to_type='counts')
+def generate_logo_plot(aligned_seq_list, output_file):
+    count_matrix = logomaker.alignment_to_matrix(aligned_seq_list, to_type='counts')
     
-    # It's up to you to decide.
-    colors = {
+    # Custom color scheme configuration
+    color_scheme = {
         'A': 'green',
         'C': 'blue',
         'D': 'red',
@@ -65,23 +65,21 @@ def create_figure(sequence_list, output_filename):
     }
     
     plt.figure(figsize=(90, 30))
-    logo_fig = logomaker.Logo(counts_df, color_scheme=colors)
-    plt.savefig(output_filename)
+    logo_plot = logomaker.Logo(count_matrix, color_scheme=color_scheme)
+    plt.savefig(output_file)
     plt.show()
 
 if __name__ == "__main__":
-    # demo
+    # Demonstration section
     random.seed(42)
     
-    # Load data
-    df = pd.read_csv("data.csv")
+    # Load input data
+    input_df = pd.read_csv("data.csv")
     
-    # Randomly sample 250 sequences
-    data1 = df.sample(n=250)
-    data1 = read_data(data1)
-    aligned_data = alignment_process(data1)
-    aligned_df = build_df(aligned_data)
-    create_figure(aligned_df, "figure.png")
-    print("Finished!")
-    
-
+    # Random subset selection
+    sampled_data = input_df.sample(n=250)
+    seq_records = generate_seq_records(sampled_data)
+    alignment_result = perform_alignment(seq_records)
+    processed_sequences = extract_aligned_sequences(alignment_result)
+    generate_logo_plot(processed_sequences, "figure.png")
+    print("Process completed successfully!")

63dd9746088f6645d598b10275c4f40fa4556358
shiipl <gm18305280617@gmail.com>
2025-03-16 11:58:07 +0800

    update

--- datasets/aa_sequence_visual.py
+++ datasets/aa_sequence_visual.py
@@ -1,25 +1,26 @@
 """
 This script is designed to analyze and visualize amino acid sequence data.
 """
+import os
 import random
+import logomaker
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from Bio import AlignIO
-from Bio.Align import MultipleSeqAlignment
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-import logomaker
-import os
+from Bio.Align import MultipleSeqAlignment
 
-def read_sequences_from_csv(df):
+
+def read_data(df):
     sequences = []
     for index, row in df.iterrows():
         sequence = SeqRecord(Seq(row['vh']), id=str(index))
         sequences.append(sequence)
     return sequences
 
-def perform_alignment(sequences):
+def alignment_process(sequences):
     with open("temp.fasta", "w") as temp_file:
         for sequence in sequences:
             temp_file.write(f">{sequence.id}\n")
@@ -30,13 +31,13 @@ def perform_alignment(sequences):
     alignment = AlignIO.read("aln.fasta", "fasta")
     return alignment
 
-def create_df(alignment):
+def build_df(alignment):
     sequences = []
     for record in alignment:
         sequences.append(str(record.seq))
     return sequences
 
-def create_logo(sequence_list, output_filename):
+def create_figure(sequence_list, output_filename):
     counts_df = logomaker.alignment_to_matrix(sequence_list, to_type='counts')
     
     # It's up to you to decide.
@@ -77,10 +78,10 @@ if __name__ == "__main__":
     
     # Randomly sample 250 sequences
     data1 = df.sample(n=250)
-    data1 = read_sequences_from_csv(data1)
-    aligned_data = perform_alignment(data1)
-    aligned_df = create_df(aligned_data)
-    create_logo(aligned_df, "output1.png")
-    print("1 Finished!")
+    data1 = read_data(data1)
+    aligned_data = alignment_process(data1)
+    aligned_df = build_df(aligned_data)
+    create_figure(aligned_df, "figure.png")
+    print("Finished!")
     
 
